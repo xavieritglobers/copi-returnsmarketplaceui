50cf85c70769dfaa4aa1dc9a8c4dbe18376a2666
santiagoaviladev <xavier0823@gmail.com>
2021-09-13 18:18:30 -0500

    fix

--- react/Components/DisplayData.tsx
+++ react/Components/DisplayData.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import React from 'react'
 import { connect } from 'react-redux';
 import ReportTable from './ReportTable'
 
